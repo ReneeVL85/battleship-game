9b7a440cd1941ed4fb89b4de1d7585d378097eb2
ReneeVL85 <renee.v.leijen@gmail.com>
2016-05-03 20:25:42 +0200

    added Canvas Component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import CanvasComponent from "./components/CanvasComponent"
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<CanvasComponent/>, document.getElementById('container'));
