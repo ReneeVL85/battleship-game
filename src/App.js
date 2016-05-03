304544ed9fa43f59930c2ebad13cc43699e10ebf
ReneeVL85 <renee.v.leijen@gmail.com>
2016-05-03 19:54:05 +0200

    added GameListComponent NewGameComponent and Utils

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import GameModel from './models/GameModel';
+import GameListComponent from "./components/GameListComponent";
+import NewGameComponent from "./components/NewGameComponent"
+
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.games = new GameModel();
+    this.games.subscribe(this.updateList.bind(this));
+
+    this.state = {
+      games: [],
+      currentGame: null
+    };
+  }
+
+  updateList(){
+    this.setState({
+      games: this.games.resources
+    })
+  }
+
+  createGame(newPlayer) {
+    this.games.addresources({
+    playerOne: newPlayer
+    });
+  }
+
     render() {
         return (
-            <h1>Hello World!</h1>
+            <div>
+              <h1>battleship</h1>
+              <NewGameComponent onCreate={this.createGame.bind(this)}/>
+              <GameListComponent games={this.state.games}/>
+            </div>
         );
     }
 }
