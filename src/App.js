31fd9ec62adbca46f987d86931b4918a8cacb589
ReneeVL85 <renee.v.leijen@gmail.com>
2016-05-04 14:11:24 +0200

    added the grid

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import BaseModel from "./models/BaseModel"
 import GameModel from './models/GameModel';
 import GameListComponent from "./components/GameListComponent";
 import NewGameComponent from "./components/NewGameComponent";
 import GridComponent from "./components/GridComponent";
+import Inventory from "./components/Inventory";
+
+const boardStyle = {
+  width: 400,
+  height: 800,
+  border: "1px solid #000",
+  margin: "auto"
+};
 
 
 class App extends React.Component {
@@ -12,9 +21,16 @@ class App extends React.Component {
     this.games = new GameModel();
     this.games.subscribe(this.updateList.bind(this));
 
+    this.grid = [];
+    for (let i=0; i < 200; i++) {
+    this.grid.push(i);
+    };
+
     this.state = {
       games: [],
-      currentGame: null
+      currentGame: null,
+      boats: [1,2,14,15,16]
+
     };
   }
 
@@ -30,13 +46,57 @@ class App extends React.Component {
     });
   }
 
+  cellHasBoat(cell) {
+  let hasBoat = this.state.boats.filter(function(boat) {
+    if (boat === cell) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+
+    return hasBoat.length > 0;
+  }
+
+  getBoatIndex(cell) {
+    if (this.cellHasBoat(cell)) {
+      return this.state.boats.findIndex(function(boat) {
+        return boat === cell;
+      });
+    }
+  }
+
+  addBoat(cell) {
+    let boats = this.state.boats;
+
+    if (this.cellHasBoat(cell)) {
+      let index = this.getBoatIndex(cell);
+      boats.splice(index, 1);
+    } else {
+      boats.push(cell);
+    }
+
+    this.setState({
+      boats: boats
+    });
+  }
+
+
+
     render() {
+      console.log("haihai")
         return (
             <div>
-              <h1>battleship</h1>
+              <h1>Battleship</h1>
               <NewGameComponent onCreate={this.createGame.bind(this)}/>
               <GameListComponent games={this.state.games}/>
-              <GridComponent />
+              <div>
+              <Inventory boats={this.state.boats}/>
+                <div style={boardStyle}>
+                {this.grid.map(function(key) {
+                  return(<GridComponent key={key} boat={this.cellHasBoat(key)} index={key} onClick={this.addBoat.bind(this)}/>);}.bind(this))}
+                </div>
+              </div>);
             </div>
         );
     }
